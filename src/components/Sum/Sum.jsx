f5822990ee1c142a625d2aaabae2837b9a2bb8f3
Igor Tinyayev <tinyayev.ca@gmail.com>
2025-10-31 07:17:53 -0600

    react_sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,7 @@
 // export const Sum = () => ();
+import React from 'react';
+
+// Named export (NOT default)
+export function Sum({ a = 0, b = 0 }) {
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
+}
